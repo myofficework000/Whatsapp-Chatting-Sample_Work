fa029bb8a042cd98ecf1b0474103fdf2992a7c62
phein <p4hein@gmail.com>
2023-02-06 08:32:45 -0600

    Implement fragment_chat.xml and chatFragment

--- app/src/main/java/com/example/whatsappfeatureapp/view/fragment/ChatFragment.kt
+++ app/src/main/java/com/example/whatsappfeatureapp/view/fragment/ChatFragment.kt
@@ -5,12 +5,20 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.whatsappfeatureapp.R
+import com.example.whatsappfeatureapp.databinding.FragmentChatBinding
+import com.example.whatsappfeatureapp.model.Chat
+import com.example.whatsappfeatureapp.model.ChatDataSource
+import com.example.whatsappfeatureapp.view.adapter.ChatAdapter
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
+private lateinit var binding : FragmentChatBinding
+private lateinit var recyclerView: RecyclerView
 
 /**
  * A simple [Fragment] subclass.
@@ -18,18 +26,6 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ChatFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
-
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -37,24 +33,11 @@ class ChatFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_chat, container, false)
     }
-
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ChatFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ChatFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val layoutManager = LinearLayoutManager(context)
+        recyclerView = view.findViewById(R.id.recyclerView)
+        binding.recyclerView.layoutManager = layoutManager
+        binding.recyclerView.adapter = ChatAdapter(ChatDataSource.getChatList())
     }
 }
